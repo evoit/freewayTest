77f5d5d121cec498e05c30abbe1e3389c4caad68
wko-test <wko@optionscity.com>
2017-01-18 17:15:30 -0600

    created demo job

--- algo/src/DemoJob.java
+++ algo/src/DemoJob.java
@@ -1,20 +1,33 @@
 import com.optionscity.freeway.api.AbstractJob;
 import com.optionscity.freeway.api.IContainer;
 import com.optionscity.freeway.api.IJobSetup;
+import com.optionscity.freeway.api.Prices;
+import com.optionscity.freeway.api.messages.MarketBidAskMessage;
 
 /**
  * Created by demo01 on 1/11/2017.
  */
 public class DemoJob extends AbstractJob{
+    String instr;
     @Override
-    public void install(IJobSetup iJobSetup) {
-
+    public void install(IJobSetup setup) {
+        setup.addVariable("Instrument","Instrument to configure job on","instrument","");
     }
     public void begin(IContainer container) {
         super.begin(container);
-        log("Hello world!");
+        container.subscribeToMarketBidAskMessages();
+        instr = container.getVariable("Instrument");
+        log(instr);
+        container.filterMarketMessages(instr);
     }
     public void onTimer(){
         debug("timer invoked every second");
     }
+    public void onMarketBidAsk(MarketBidAskMessage msg) {
+        Prices topOfBook = instruments().getTopOfBook(msg.instrumentId);
+        log(msg.instrumentId);
+        log("Bid is " + topOfBook.bid + " Qty is " + topOfBook.bid_size);
+        log("Ask is " +topOfBook.ask + " Qty is " + topOfBook.ask_size);
+    }
+
 }
