24480b026cf323c2cf030d558025810cbe07f9c0
Erik Voit <evoit@optionscity.com>
2017-01-23 23:18:39 -0600

    Added underlying price testing

--- algo/src/TrackerJob.java
+++ algo/src/TrackerJob.java
@@ -1,6 +1,8 @@
 import com.optionscity.freeway.api.AbstractJob;
 import com.optionscity.freeway.api.IContainer;
 import com.optionscity.freeway.api.IJobSetup;
+import com.optionscity.freeway.api.Prices;
+import com.optionscity.freeway.api.messages.MarketBidAskMessage;
 import com.optionscity.freeway.api.messages.MarketLastMessage;
 
 /**
@@ -10,13 +12,13 @@ public class TrackerJob extends AbstractJob {
     String instrumentID;
     String choice;
     double lastPrice;
+    double underlyingPrice;
 
 
     @Override
     public void install(IJobSetup iJobSetup) {
         iJobSetup.addVariable("Instrument", "Instrument to Track", "instrument", "");
         iJobSetup.addVariable("TrackedMetric", "Implied Vol or Last Trade", "choice:Implied Vol;Last Trade", "Implied Vol");
-
     }
     public void begin (IContainer container){
         super.begin(container);
@@ -32,6 +34,18 @@ public class TrackerJob extends AbstractJob {
         if ("Last Trade".equals(choice)) {
             lastPrice = msg.price;
             log("last price is: " + lastPrice);
+        } else {
+            lastPrice = msg.price;
+            log("last price is: " + lastPrice);
+            //Prices prices=instruments().getMarketPrices(instrumentID);
+            //underlyingPrice= prices.getUnderlyingMidMarket();
+            underlyingPrice = instruments().getMarketPrices(instrumentID).getUnderlyingMidMarket();
+            log("underlying price is: " + underlyingPrice);
         }
     }
+    /*public void onMarketBidAsk(MarketBidAskMessage m) {
+        Prices prices=instruments().getMarketPrices(m.instrumentId);
+        double underlyingPrice= prices.getUnderlyingMidMarket();
+        log("underlying price is: " + underlyingPrice);
+    }*/
 }
