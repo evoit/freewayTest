c0806acbcffb57472d360351868fa19ecd498442
Erik Voit <evoit@optionscity.com>
2017-01-25 23:00:51 -0600

    didn't start trackerjob
    Conflicts:
    	algo/src/TrackerJob.java

--- algo/src/TrackerJob.java
+++ algo/src/TrackerJob.java
@@ -17,13 +17,19 @@ public class TrackerJob extends AbstractJob {
     double lastPrice;
     double underlyingPrice;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1ed7e63... Sstedd trackerjob
     double optionMid;
     double futureMid;
     double impliedVol;
     Prices futurePrices;
     Prices optionPrices;
+<<<<<<< HEAD
 =======
 >>>>>>> 121877f... Added underlying price testing
+=======
+>>>>>>> 1ed7e63... Sstedd trackerjob
 
 
     @Override
@@ -58,13 +64,53 @@ public class TrackerJob extends AbstractJob {
         if ("Last Trade".equals(choice)) {
             lastPrice = msg.price;
             log("last price is: " + lastPrice);
-        } else {
+        }
+
+        //TODO update grid with last
+
+        /*else {
             lastPrice = msg.price;
             log("last price is: " + lastPrice);
             //Prices prices=instruments().getMarketPrices(instrumentID);
             //underlyingPrice= prices.getUnderlyingMidMarket();
             underlyingPrice = instruments().getMarketPrices(instrumentID).getUnderlyingMidMarket();
             log("underlying price is: " + underlyingPrice);
+        }*/
+    }
+
+    public void onMarketBidAsk(MarketBidAskMessage m) {
+        if ("Implied Vol".equals(choice) && !isPricesTheSame(m.instrumentId)) {
+            if (m.instrumentId.equals(instrumentID)) {
+                double mid = getCleanMidMkt(m.instrumentId);
+                if (!Double.isNaN(mid)) {
+                    optionMid = mid;
+                }
+            } else {
+                double mid = getCleanMidMkt(m.instrumentId);
+                if (!Double.isNaN(mid)){
+                    futureMid = mid;
+                }
+            }
+            updateVol();
+        }
+    }
+
+    private void updateVol() {
+        impliedVol = theos().calculateImpliedVolatility(instrumentID, optionMid, futureMid);
+
+        //TODO update grid with last implied vol
+    }
+
+    private double getCleanMidMkt (String instrumentID) {
+        Prices prices = instruments().getMarketPrices(instrumentID);
+        if (Double.isNaN(prices.bid) && Double.isNaN(prices.ask)) {
+            return Double.NaN;
+        } else if (Double.isNaN(prices.ask)) {
+            return prices.bid;
+        } else if (Double.isNaN(prices.bid)) {
+            return prices.ask;
+        } else {
+            return 0.5*(prices.ask + prices.bid);
         }
 
         //TODO update grids
@@ -129,9 +175,19 @@ public class TrackerJob extends AbstractJob {
         Prices prices=instruments().getMarketPrices(instrumentId);
         return prices.bid == lastTrackedPrices.bid && prices.ask == lastTrackedPrices.ask;
     }
-    /*public void onMarketBidAsk(MarketBidAskMessage m) {
-        Prices prices=instruments().getMarketPrices(m.instrumentId);
-        double underlyingPrice= prices.getUnderlyingMidMarket();
-        log("underlying price is: " + underlyingPrice);
-    }*/
+
+
+    private boolean isPricesTheSame(String instrumentId){
+        Prices lastTrackedPrices;
+        if (instrumentId.equals(instrumentID)) {
+            lastTrackedPrices = optionPrices;
+        } else if (instrumentId.equals(underlyingInstrumentId)) {
+            lastTrackedPrices = futurePrices;
+        } else {
+            lastTrackedPrices = new Prices();
+        }
+
+        Prices prices=instruments().getMarketPrices(instrumentId);
+        return prices.bid == lastTrackedPrices.bid && prices.ask == lastTrackedPrices.ask;
+    }
 }
